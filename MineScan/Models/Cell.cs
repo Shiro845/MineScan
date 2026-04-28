2d776e162b45314fbf64d4bcf2bc57357a842c19
Shiro845 <fvsandking@gmail.com>
2026-04-29 00:18:40 +0300

    Initial commit

---
+++ MineScan/Models/Cell.cs
@@ -0,0 +1,9 @@
+﻿namespace MineScan.Models;
+
+public struct Cell
+{
+    public bool IsMine { get; set; }
+    public bool IsOpen { get; set; }
+    public bool IsFlagged  { get; set; }
+    public bool MinesAround  { get; set; }
+}
---
+++ MineScan/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MineScan.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+    
+}
